385c38f6f51847f35d4bb095581ef9c2714c03a4
iqbal7230 <iqbalxply@gmail.com>
2025-09-15 20:56:12 +0530

    deployed

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,18 @@ const port = process.env.PORT || 5000;
 
 // to parse req body
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "https://examguardian.onrender.com",
+      "https://exam-guardian-three.vercel.app",
+      "http://localhost:5173",
+    ],
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
